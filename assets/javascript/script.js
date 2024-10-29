c9d54e66edb2283fef4184fa6980c931cc2653c0
Aki <s.aki.w@proton.me>
2024-10-29 11:17:27 +0900

    Got rid of cursor ring

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -232,41 +232,41 @@ function closeNav() {
 }
 
 // cursor
-let cursor = document.querySelector('.cursor');
-let cursorScale = document.querySelectorAll('a,button,.pop-up,.trigger,.share,#close,.toggle,#vimeo,#youtube,.link,.gallery');
-let mouseX = 0;
-let mouseY = 0;
+// let cursor = document.querySelector('.cursor');
+// let cursorScale = document.querySelectorAll('a,button,.pop-up,.trigger,.share,#close,.toggle,#vimeo,#youtube,.link,.gallery');
+// let mouseX = 0;
+// let mouseY = 0;
 
-gsap.to({}, 0.016, {
-    repeat: -1,
-    onRepeat: function () {
-        gsap.set(cursor, {
-            css: {
-                left: mouseX,
-                top: mouseY,
-            }
-        })
-    }
-});
+// gsap.to({}, 0.016, {
+//     repeat: -1,
+//     onRepeat: function () {
+//         gsap.set(cursor, {
+//             css: {
+//                 left: mouseX,
+//                 top: mouseY,
+//             }
+//         })
+//     }
+// });
 
-window.addEventListener('mousemove', (e) => {
-    mouseX = e.clientX;
-    mouseY = e.clientY;
-})
-cursorScale.forEach(link => {
-    link.addEventListener('mousemove', () => {
-        cursor.classList.add('grow');
-        if (link.classList.contains('small')) {
-            cursor.classList.remove('grow');
-            cursor.classList.add('grow-small');
-        }
-    });
+// window.addEventListener('mousemove', (e) => {
+//     mouseX = e.clientX;
+//     mouseY = e.clientY;
+// })
+// cursorScale.forEach(link => {
+//     link.addEventListener('mousemove', () => {
+//         cursor.classList.add('grow');
+//         if (link.classList.contains('small')) {
+//             cursor.classList.remove('grow');
+//             cursor.classList.add('grow-small');
+//         }
+//     });
 
-    link.addEventListener('mouseleave', () => {
-        cursor.classList.remove('grow');
-        cursor.classList.remove('grow-small');
-    });
-});
+//     link.addEventListener('mouseleave', () => {
+//         cursor.classList.remove('grow');
+//         cursor.classList.remove('grow-small');
+//     });
+// });
 
 // Whole Page Scrolling Animation
 const observer = new IntersectionObserver((entries) => {
